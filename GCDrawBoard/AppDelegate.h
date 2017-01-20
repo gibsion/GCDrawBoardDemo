5fec6fed712e6038e6acf6efe7b02eefcdd7b290
Gibsion <gibsion@aliyun.com>
2017-01-20 11:32:32 +0800

    Initial Commit

---
+++ GCDrawBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCDrawBoard
+//
+//  Created by APPLE on 2017/1/20.
+//  Copyright © 2017年 Gibson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
