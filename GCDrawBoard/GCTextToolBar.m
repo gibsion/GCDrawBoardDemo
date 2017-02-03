54d6cacd896e91a41e128cf87004eecb69054e8b
Gibsion <gibsion@aliyun.com>
2017-02-03 23:43:47 +0800

    文本工具栏添加字体颜色选择

--- GCDrawBoard/GCTextToolBar.m
+++ GCDrawBoard/GCTextToolBar.m
@@ -7,13 +7,14 @@
 //
 
 #import "GCTextToolBar.h"
+#import "GCColorSelectView.h"
 #import "YBPopupMenu.h"
 
 #define GCDefaultText   @"abc"
 #define GCFontCellIdentifier        @"GCFontCellIdentifier"
 
-@interface GCTextToolBar () <UITableViewDelegate, UITableViewDataSource, YBPopupMenuDelegate>
-
+@interface GCTextToolBar () <YBPopupMenuDelegate>
+//subviews
 @property (strong, nonatomic) UILabel *currentFontTextLabel;
 
 @property (strong, nonatomic) UIButton *btnFontSize;
@@ -22,8 +23,9 @@
 
 @property (strong, nonatomic) UIButton *btnFontColorSelect;
 
-@property (strong, nonatomic) UITableView *tableView;
+@property (strong, nonatomic) GCColorSelectView *colorSelectView;
 
+//Data
 @property (strong, nonatomic) NSMutableArray *textArray;
 
 @property (strong, nonatomic) NSMutableArray *fontSizeArray;
@@ -50,37 +52,54 @@
     [self addSubview: self.currentFontTextLabel];
     [self addSubview: self.btnFontSize];
     [self addSubview: self.btnFontSelect];
-    
-    NSArray *familys = [UIFont familyNames];
-    for (NSString *family in familys) {
-        NSArray *names = [UIFont fontNamesForFamilyName: family];
-        for (NSString *name in names) {
-            NSLog(@"+++++: %@", name);
-        }
-    }
+    [self addSubview: self.btnFontColorSelect];
 }
 
 -(void)initData {
     self.fontSizeArray = [[NSMutableArray alloc] init];
-    for (NSInteger i = 10; i <= 40; i++) {
+    for (NSInteger i = 10; i <= 60; i++) {
         NSString *text = [NSString stringWithFormat: @"%zd", i];
         [self.fontSizeArray addObject: text];
     }
     
     self.fontArray = [[NSMutableArray alloc] init];
-    [self.fontArray addObject: [UIFont fontWithName: @"Copperplate-Light" size: 15]];
-    [self.fontArray addObject: [UIFont fontWithName: @"Copperplate-Bold" size: 15]];
-    [self.fontArray addObject: [UIFont fontWithName: @"Thonburi-Light" size: 15]];
-    [self.fontArray addObject: [UIFont fontWithName: @"Thonburi-Bold" size: 15]];
-    [self.fontArray addObject: [UIFont fontWithName: @"AppleColorEmoji" size: 15]];
-    [self.fontArray addObject: [UIFont fontWithName: @"MarkerFelt-Thin" size: 15]];
-    [self.fontArray addObject: [UIFont fontWithName: @"MarkerFelt-Wide" size: 15]];
-    [self.fontArray addObject: [UIFont fontWithName: @"Zapfino" size: 15]];
+    //中文
+    [self.fontArray addObject: [UIFont boldSystemFontOfSize: 18]];
+    [self.fontArray addObject: [UIFont fontWithName: @"DFWaWaSC-W5" size: 18]]; //娃娃体
+    //外语
+    [self.fontArray addObject: [UIFont fontWithName: @"Zapfino" size: 18]];
+    [self.fontArray addObject: [UIFont fontWithName: @"Papyrus" size: 18]];
+    [self.fontArray addObject: [UIFont fontWithName: @"Papyrus-Condensed" size: 18]];
+    [self.fontArray addObject: [UIFont fontWithName: @"BodoniOrnamentsITCTT" size: 18]];
+    [self.fontArray addObject: [UIFont fontWithName: @"SavoyeLetPlain" size: 18]];
+    [self.fontArray addObject: [UIFont fontWithName: @"SnellRoundhand-Bold" size: 18]];
+    [self.fontArray addObject: [UIFont fontWithName: @"SnellRoundhand" size: 18]];
+    [self.fontArray addObject: [UIFont fontWithName: @"SnellRoundhand-Black" size: 18]];
+    [self.fontArray addObject: [UIFont fontWithName: @"Cochin-BoldItalic" size: 18]];
+    [self.fontArray addObject: [UIFont fontWithName: @"Trebuchet-BoldItalic" size: 18]];
+    [self.fontArray addObject: [UIFont fontWithName: @"BradleyHandITCTT-Bold" size: 18]];
+    [self.fontArray addObject: [UIFont fontWithName: @"HoeflerText-BlackItalic" size: 18]];
+    [self.fontArray addObject: [UIFont fontWithName: @"Chalkduster" size: 18]];
+    [self.fontArray addObject: [UIFont fontWithName: @"PartyLetPlain" size: 18]];
+    [self.fontArray addObject: [UIFont fontWithName: @"Noteworthy-Bold" size: 18]];
+    [self.fontArray addObject: [UIFont fontWithName: @"ChalkboardSE-Bold" size: 18]];
+    [self.fontArray addObject: [UIFont fontWithName: @"MarkerFelt-Thin" size: 18]];
+    [self.fontArray addObject: [UIFont fontWithName: @"MarkerFelt-Wide" size: 18]];
+    
+//    UIFont *font = [UIFont boldSystemFontOfSize: 15];
+//    NSLog(@"boldSystemFont : %@", font.fontName);
+//    NSArray *familys = [UIFont familyNames];
+//    for (NSString *family in familys) {
+//        NSArray *names = [UIFont fontNamesForFamilyName: family];
+//        for (NSString *name in names) {
+//            NSLog(@"+++++: %@", name);
+//            [self.fontArray addObject: [UIFont fontWithName: name size: 15]];
+//        }
+//    }
 }
 
 -(void)selectFonSize:(UIButton *)sender {
     [self.textArray removeAllObjects];
-//    [self.tableView removeFromSuperview];
     self.currentTableStyle = 0;
     
     self.textArray = [self.fontSizeArray mutableCopy];
@@ -89,19 +108,15 @@
                           icons: nil
                       menuWidth: 60
                        delegate: self];
-    
-//    self.tableView.center = CGPointMake(sender.center.x, -(CGRectGetHeight(self.tableView.frame)) / 2.0);
-//    [self addSubview: self.tableView];
 }
 
 -(void)selectFont:(UIButton *)sender {
     [self.textArray removeAllObjects];
-//    [self.tableView removeFromSuperview];
     self.currentTableStyle = 1;
     
     for (NSInteger i = 0; i < self.fontArray.count; i++) {
-        UIFont *font = [self.fontArray[i] fontWithSize: self.btnFontSize.titleLabel.text.floatValue];
-        NSAttributedString *text = [[NSAttributedString alloc] initWithString: GCDefaultText
+        UIFont *font = self.fontArray[i];//[self.fontArray[i] fontWithSize: self.btnFontSize.titleLabel.text.floatValue];
+        NSAttributedString *text = [[NSAttributedString alloc] initWithString: [NSString stringWithFormat: @"%@", font.fontName]
                                                                   attributes: @{NSFontAttributeName             : font,
                                                                                 NSForegroundColorAttributeName  : [UIColor blackColor]}];
         [self.textArray addObject: text];
@@ -109,11 +124,24 @@
     
     [YBPopupMenu showRelyOnView: sender attributedTitles: self.textArray
                           icons: nil
-                      menuWidth: 100
+                      menuWidth: 300
                        delegate: self];
-    
-//    self.tableView.center = CGPointMake(sender.center.x, -(CGRectGetHeight(self.tableView.frame)) / 2.0);
-//    [self addSubview: self.tableView];
+}
+
+-(void)selectFontColor:(UIButton *)sender {
+    if (_colorSelectView) {
+        [self removeColorSelectView];
+    }
+    else {
+        [self addSubview: self.colorSelectView];
+    }
+}
+
+-(void)removeColorSelectView {
+    if (_colorSelectView) {
+        [_colorSelectView removeFromSuperview];
+        _colorSelectView = nil;
+    }
 }
 
 #pragma getter & setter
@@ -134,7 +162,7 @@
 
 -(UIButton *)btnFontSize {
     if (!_btnFontSize) {
-        _btnFontSize = [[UIButton alloc] initWithFrame: CGRectMake(CGRectGetMaxX(self.currentFontTextLabel.frame), 0, 30, CGRectGetHeight(self.frame))];
+        _btnFontSize = [[UIButton alloc] initWithFrame: CGRectMake(CGRectGetMaxX(self.currentFontTextLabel.frame) + 10, 0, 30, CGRectGetHeight(self.frame))];
         [_btnFontSize setTitle: [NSString stringWithFormat: @"%g", self.currentFont.pointSize] forState: UIControlStateNormal];
         [_btnFontSize setTitleColor: [UIColor whiteColor] forState: UIControlStateNormal];
         [_btnFontSize addTarget: self action: @selector(selectFonSize:) forControlEvents: UIControlEventTouchUpInside];
@@ -145,7 +173,7 @@
 
 -(UIButton *)btnFontSelect {
     if (!_btnFontSelect) {
-        _btnFontSelect = [[UIButton alloc] initWithFrame: CGRectMake(CGRectGetMaxX(self.btnFontSize.frame) + 10, 0, 40, CGRectGetHeight(self.frame))];
+        _btnFontSelect = [[UIButton alloc] initWithFrame: CGRectMake(CGRectGetMaxX(self.btnFontSize.frame) + 10, 0, 30, CGRectGetHeight(self.frame))];
         [_btnFontSelect setTitle: @"A" forState: UIControlStateNormal];
         _btnFontSelect.titleLabel.font = [UIFont boldSystemFontOfSize: 20];
         [_btnFontSelect addTarget: self action: @selector(selectFont:) forControlEvents: UIControlEventTouchUpInside];
@@ -154,22 +182,34 @@
     return _btnFontSelect;
 }
 
--(UITableView *)tableView {
-    if (!_tableView) {
-        _tableView = [[UITableView alloc] initWithFrame: CGRectMake(0, 0, 60, 160)];
-        _tableView.backgroundColor = [UIColor yellowColor];
-        _tableView.userInteractionEnabled = YES;
-        _tableView.delegate = self;
-        _tableView.dataSource = self;
-        _tableView.scrollEnabled = YES;
-        _tableView.showsVerticalScrollIndicator = NO;
-        _tableView.separatorColor = [UIColor lightGrayColor];
-        _tableView.separatorInset = UIEdgeInsetsZero;
-        _tableView.tableHeaderView = [UIView new];
-        _tableView.tableFooterView = [UIView new];
+-(UIButton *)btnFontColorSelect {
+    if (!_btnFontColorSelect) {
+        _btnFontColorSelect = [[UIButton alloc] initWithFrame: CGRectMake(CGRectGetMaxX(self.btnFontSelect.frame) + 10, CGRectGetHeight(self.frame) / 2.0 - 15, 30, 30)];
+        _btnFontColorSelect.backgroundColor = [UIColor yellowColor];
+        _btnFontColorSelect.layer.cornerRadius = 15;
+        _btnFontColorSelect.clipsToBounds = YES;
+        [_btnFontColorSelect addTarget: self action: @selector(selectFontColor:) forControlEvents: UIControlEventTouchUpInside];
     }
     
-    return _tableView;
+    return _btnFontColorSelect;
+}
+
+-(GCColorSelectView *)colorSelectView {
+    if (!_colorSelectView) {
+        _colorSelectView = [[GCColorSelectView alloc] initWithFrame: CGRectMake(self.bounds.size.width/2.0 + 15, 0, self.bounds.size.width / 2.0 - 15*2, 30)];
+        _colorSelectView.backgroundColor = [UIColor whiteColor];
+        
+        __weak typeof(self) weakSelf = self;
+        _colorSelectView.didSelectedBlock = ^(UIColor *color) {
+            weakSelf.btnFontColorSelect.backgroundColor = color;
+            weakSelf.currentTextColor = color;
+            [weakSelf removeColorSelectView];
+            weakSelf.currentFontTextLabel.attributedText = [weakSelf sampleText];
+        };
+        
+    }
+    
+    return _colorSelectView;
 }
 
 -(UIFont *)currentFont {
@@ -194,48 +234,6 @@
     }
     
     return _textArray;
-}
-
-#pragma mark --UITableViewDelegate, UITableViewDataSource
-
--(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
-}
-
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.textArray.count;
-}
-
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: GCFontCellIdentifier];
-    if (!cell) {
-        cell = [[UITableViewCell alloc] initWithStyle: UITableViewCellStyleValue1 reuseIdentifier: GCFontCellIdentifier];
-        cell.backgroundColor = [UIColor clearColor];
-    }
-    
-    cell.textLabel.text = self.textArray[indexPath.row];
-    
-    return cell;
-}
-
--(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView cellForRowAtIndexPath: indexPath];
-    if (self.currentTableStyle == 0) {
-        self.currentFont = [self.currentFont fontWithSize: cell.textLabel.text.floatValue];
-        [self.btnFontSize setTitle: cell.textLabel.text forState: UIControlStateNormal];
-    }
-    else {
-        self.currentFont = [cell.textLabel.font fontWithSize: self.btnFontSize.titleLabel.text.floatValue];
-    }
-    
-    self.currentFontTextLabel.attributedText = [self sampleText];
-    
-//    [self.tableView removeFromSuperview];
-//    self.tableView = nil;
-}
-
--(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return 30.0f;
 }
 
 #pragma mark - YBPopupMenuDelegate
